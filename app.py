a1aafbb73183af4aa8e0cb4c8925d0c36cee7d56
oliviawyy <olivia.c@aluno.senai.br>
2026-06-18 17:29:52 -0300

    oi

--- app.py
+++ app.py
@@ -34,9 +34,36 @@ def pag_cadastro():
 def pag_produto():
      return render_template("pagina_produto.html")
 
-@app.route("/carrinho")
-def pag_carrinho():
-     return render_template("carrinho.html")
+@app.route('/carrinho')
+def ver_carrinho():
+    carrinho_sessao = session.get('carrinho', []) 
+    
+    subtotal = 0
+    itens_carrinho = []
+    
+    
+    cursor = mysql.connection.cursor()
+    for item in carrinho_sessao:
+        cursor.execute("SELECT * FROM produtos WHERE codigo = %s", [item['codigo']])
+        produto = cursor.fetchone() # Retorna um dicionário ou tupla do banco
+        
+        if produto:
+            # Junta as infos do banco com a quantidade e tamanho escolhidos
+            produto_completo = {
+                'codigo': produto['codigo'],
+                'nome': produto['nome'],
+                'foto': produto['foto'],
+                'valor': float(produto['valor']),
+                'tamanho': item['tamanho'],
+                'quantidade': item['quantidade']
+            }
+            subtotal += produto_completo['valor'] * produto_completo['quantidade']
+            itens_carrinho.append(produto_completo)
+            
+    cursor.close()
+    
+    # Renderiza passando os dados para o Jinja2 trabalhar
+    return render_template('carrinho.html', itens_carrinho=itens_carrinho, subtotal=subtotal)
 
 if __name__ == '__main__':
      app.run(debug=True)
