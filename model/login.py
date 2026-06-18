cc647ffc737571925082c1ec82aeb13383546fab
Ana Laura <ana.machioni@aluno.senai.br>
2026-06-18 17:27:55 -0300

    add: Ajustando login.py e fazendo css da pag sobre

--- model/login.py
+++ model/login.py
@@ -3,10 +3,8 @@ from database.conexao import Conexao
 class Login():
     def  login_usuario(nome, senha):
         conexao, cursor = Conexao.conectar()
-        cursor.execute("SELECT * FROM usuarios WHERE nome = %s AND senha = %s", [nome, senha])
-        return cursor.fetchone()
+        cursor.execute("INSERT INTO usuarios WHERE nome = %s AND senha = %s", [nome, senha])
         conexao.commit()
         conexao.close()
-        
     
     
