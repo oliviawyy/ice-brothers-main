fe29f04bce7a49e6a0b81f432e80c9d190ff524a
Ana Laura <ana.machioni@aluno.senai.br>
2026-06-19 07:56:24 -0300

    add: Reajustando login.py

--- model/login.py
+++ model/login.py
@@ -3,8 +3,8 @@ from database.conexao import Conexao
 class Login():
     def  login_usuario(nome, senha):
         conexao, cursor = Conexao.conectar()
-        cursor.execute("INSERT INTO usuarios WHERE nome = %s AND senha = %s", [nome, senha])
+        cursor.execute("SELECT * FROM usuarios WHERE nome = %s AND senha = %s", [nome, senha])
+        resultado = cursor.fetchall()
         conexao.commit()
         conexao.close()
-    
-    
+        return resultado
