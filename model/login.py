a1ad3db388af01b5fa0c50d3ad65909940901a08
oliviawyy <olivia.c@aluno.senai.br>
2026-06-19 10:04:23 -0300

    tenando

--- model/login.py
+++ model/login.py
@@ -5,13 +5,7 @@ class Login():
     def login_usuario(nome, senha):
         conexao, cursor = Conexao.conectar()
         cursor.execute("SELECT * FROM usuarios WHERE nome = %s AND senha = %s", [nome, senha])
-<<<<<<< HEAD
-        usuario = cursor.fetchone() # pega o usuário encontrado
-        conexao.close()
-        return usuario 
-=======
         resultado = cursor.fetchall()
         conexao.commit()
         conexao.close()
         return resultado
->>>>>>> 8e6da39f76484789e18f7a96744df4f590e4a87c
