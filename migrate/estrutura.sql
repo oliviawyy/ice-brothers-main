15407f892805e82227ef1f24114a4759079df2ee
Ana Laura <ana.machioni@aluno.senai.br>
2026-06-19 13:12:02 -0300

    add: Fazendo comentarios e colocando footer

--- migrate/estrutura.sql
+++ migrate/estrutura.sql
@@ -31,4 +31,9 @@ VALUES( 'DEEP BLUE', 'Streetwear pesado com estética premium. Tom azul royal in
 	  ( 'SKY BLUE', 'Estética clean para o topo do seu visual. Tom azul claro moderno em tecido de alta gramatura, garantindo a estrutura perfeita das marcas de gringa com máximo conforto.', '139.90', 'static/img/ChatGPT Image 28 de mai. de 2026, 16_36_39.png', 'camisas'),
       ( 'ICE WHITE', 'O clássico indispensável que manda no jogo. Base branca impecável com alto contraste no logo. Ultra versátil, combina perfeitamente com correntes e acessórios.', '139.90', 'static/img/over_branca.png', 'camisas')
     
-    
+CREATE TABLE IF NOT EXISTS comentarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    produto_codigo VARCHAR(50) NOT NULL,
+    usuario VARCHAR(100) NOT NULL,
+    texto TEXT NOT NULL
+);
